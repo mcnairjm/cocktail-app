2e9a8a5497212089e829972b66a5a37d49a0a9ef
mcnairjm19 <mcnairjm1993@gmail.com>
2021-12-27 20:31:44 -0500

    random fetch

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function fetchCocktails() {
+/*function fetchCocktails() {
     var apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
   
     fetch(apiUrl)
@@ -13,4 +13,49 @@ function fetchCocktails() {
       });
 }
   
-fetchCocktails()
+fetchCocktails()*/
+
+var randomBtn = document.getElementById('random-btn')
+
+function fetchRandomCocktail() {
+  var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+  fetch(apiUrl)
+  .then(function (res) {
+    if(!res.ok) {
+      throw Error('Error');
+    }
+    return res.json();
+  })
+  .then(function(data) {
+    console.log(data.drinks)
+    document.querySelector
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+}
+
+  /*function displayRandomCocktail(data) {
+    var cocktailDiv = document.getElementById('cocktails')
+    var cocktailName = cocktail.strDrink;
+    var heading = document.createElement('h1');
+    heading.innerHTML = cocktailName;
+    cocktailDiv.appendChild(heading);
+    var cocktailImg = document.createElement('img');
+    cocktailImg.src = cocktail.strDrinkThumb;
+    cocktailDiv.appendChild(cocktailImg);
+    document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb
+
+    var cocktailIngredients = document.createElement('ul');
+    cocktailDiv.appendChild(cocktailIngredients);
+
+}*/
+
+fetchRandomCocktail();
+
+//displayRandomCocktail();
+
+
+
