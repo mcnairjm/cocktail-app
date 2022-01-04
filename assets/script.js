45aea602025faa3074546c75141dab6fc09490c4
woffordlm <lukewofford@gmail.com>
2022-01-03 19:26:51 -0500

    changed eventlistener

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
 var searchButton = document.getElementById("search-button");
 var searchInputEl = document.getElementById("cocktail-search-id");
+var cocktailSearchContainerEl = document.getElementById("cocktail-search-container");
 var cocktailContainerEl = document.getElementById("cocktail-container");
 var cocktailZeroEl = document.getElementById("cocktail-0-id");
 var cocktailOneEl = document.getElementById("cocktail-1-id");
@@ -269,7 +270,7 @@ cocktailContainerEl.addEventListener("click", fillModal);
 // searchButton.addEventListener("click", fetchCocktails);
 randomBtn.addEventListener('click',fetchRandomCocktail);
 searchButton.addEventListener("click",fetchEventHandler);
-buttonFooterParentEl.addEventListener("click", saveCocktail);
+buttonFooterEl.addEventListener("click", saveCocktail);
 clearHistoryEl.addEventListener("click", clearHistory);
 historyEl.addEventListener("click", fillModal)
 
